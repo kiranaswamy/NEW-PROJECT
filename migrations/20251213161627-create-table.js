685945471ebdba7a28ba7789a750f04574d9f52c
kiranaswamy <kiranswamygowda72@gmail.com>
2025-12-13 22:17:05 +0530

    migration

---
+++ migrations/20251213161627-create-table.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.createTable('Member',{
+      id:{
+        type:Sequelize.INTEGER,
+        primaryKey:true,
+        autoIncrement:true
+      },
+      username:{
+        type:Sequelize.STRING
+      }
+    })
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.dropTable('Member');
+  }
+};
